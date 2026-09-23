ce1f5201dc75bf9871b0c8bfb00e13d75a6c92fa
Christophe Tarillon <christophetarillon@gmail.com>
2026-09-23 17:01:29 +0200

    Move 'Prélèvement' to second-to-last in the payment type list

--- apps/web/src/components/accounting/EntryForm.tsx
+++ apps/web/src/components/accounting/EntryForm.tsx
@@ -429,8 +429,8 @@ export default function EntryForm({ seasonId, userId, onSuccess }: EntryFormProp
           <option value="virement">Virement</option>
           <option value="cheque">Chèque</option>
           <option value="cb">Carte bancaire</option>
-          <option value="prelevement">Prélèvement</option>
           <option value="especes">Espèces</option>
+          <option value="prelevement">Prélèvement</option>
           <option value="autre">Autre</option>
         </select>
       </div>
